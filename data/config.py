71068055932833e6e032d7f1d3c5c7162212c63d
Maksim Tuchka <maksim.tuchka@yahoo.com>
2022-08-19 18:06:06 +0300

    fixed db

--- data/config.py
+++ data/config.py
@@ -4,9 +4,12 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = "5723874330:AAEzSncJrJRGxm7l-0vnZrzZz87LV3mU_ZA"
+ADMINS = [178875921]
+IP = 'localhost'
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
+# ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+# IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
 dp_api = {
   "type": "service_account",
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,4 +1,5 @@
 from . import start
 from . import send
+from . import get
 from . import other_message
 
