78f9270282fd4e96cc8157ec0dfecd51318e2b9a
Maksim Tuchka <82935703+MrTuchka@users.noreply.github.com>
2022-10-04 14:41:57 +0300

    Update set_bot_commands.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -13,10 +13,10 @@ async def set_default_commands(dp):
     )
 
 #Команди, які бачать тільки адмінітсратори
-    for admin in ADMINS:
-        await dp.bot.set_my_commands(
-        [
-            types.BotCommand("start", str(db.get("start_command"))),
-            types.BotCommand("send", str(db.get("send_command"))),
-        ], BotCommandScopeChat(chat_id=admin)
-    )
+#    for admin in ADMINS:
+#        await dp.bot.set_my_commands(
+#        [
+#            types.BotCommand("start", str(db.get("start_command"))),
+#            types.BotCommand("send", str(db.get("send_command"))),
+#        ], BotCommandScopeChat(chat_id=admin)
+#    )
